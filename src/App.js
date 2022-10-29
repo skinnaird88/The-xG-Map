66710b84964974bb84eebf1df729e42a6106eda8
Stephen Kinnaird <slkinnaird@gmail.com>
2022-10-29 12:25:50 +0100

    router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import logo from './logo.svg';
+import Dashboard from "./containers/Dashboard"
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Header/>
-      <NavBar/>
-      <Routes>
-        <Route></Route>
-      </Routes>
-      <Pitch/>
-      <Form/>
-      <Footer/>
+      <Router>
+        <Routes>
+          
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/containers/Dashboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>Dashboard
+        <h1>hi</h1>
+    </div>
+  )
+}
+export default Dashboard
