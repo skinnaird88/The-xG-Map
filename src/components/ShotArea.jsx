ac18ae764eccaeb483411630d03a5cf14cfc63c2
Stephen Kinnaird <slkinnaird@gmail.com>
2022-10-29 15:54:22 +0100

    grid experiements

--- src/components/ShotArea.jsx
+++ src/components/ShotArea.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import './ShotArea.css'
 
 const ShotArea = () => {
   return (
-    <></>
+    <>
+    <div className="wrapper">
+    <div id="defenders">1</div>
+    <div id='shot'>2</div>
+    <div id='goal'>3</div>
+    </div>
+    </>
     )
 }
 
