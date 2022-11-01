e9ab02240ef3f16c4a6f6eeebd5553dcb577d6fe
Stephen Kinnaird <slkinnaird@gmail.com>
2022-11-01 11:16:49 +0000

    server booted

---
+++ server/src/main/java/com/example/thexgmapservice/ThexgmapserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.thexgmapservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThexgmapserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThexgmapserviceApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/example/thexgmapservice/ThexgmapserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thexgmapservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThexgmapserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
