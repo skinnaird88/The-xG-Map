8277ef800c7c0e48fa15ca4339a43385f64f606c
Stephen Kinnaird <slkinnaird@gmail.com>
2022-11-01 12:12:07 +0000

    team and player tables

--- server/src/main/java/com/example/thexGMapService/models/Player.java
+++ server/src/main/java/com/example/thexGMapService/models/Player.java
@@ -1,18 +1,54 @@
 package com.example.thexGMapService.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "players")
 public class Player {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
     private String name;
 
-    private int Age;
+    @Column(name = "age")
+    private int age;
 
+
+    @Column(name = "goals")
+    private Integer goals;
+
+    @Column(name = "xG")
+    private double expectedGoals;
+
+    @ManyToOne
+    @JoinColumn(name = "team_id", nullable = false)
     private Team team;
 
-    public Player(String name, int age) {
+    public Player(String name, int age, Team team) {
         this.name = name;
-        Age = age;
+        this.age = age;
+        this.team = team;
     }
 
     public Player() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
--- server/src/main/java/com/example/thexGMapService/models/Team.java
+++ server/src/main/java/com/example/thexGMapService/models/Team.java
@@ -1,17 +1,47 @@
 package com.example.thexGMapService.models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Table(name = "teams")
 public class Team {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
     private String name;
 
+    @JsonBackReference
+    @OneToMany(mappedBy = "team", fetch = FetchType.LAZY)
     private List<Player> players;
 
     public Team(String name) {
         this.name = name;
+        this.players = new ArrayList<>();
     }
 
     public Team() {
     }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<Player> players) {
+        this.players = players;
+    }
+
 }
